ed387c692bb18b69c752f0abec43b6f08c99ce46
Rhys Milling <rhys.milling@googlemail.com>
2020-02-06 22:41:37 +0000

    Create stream.sh

---
+++ stream.sh
@@ -0,0 +1,1 @@
+~/bin/ffmpeg -format_code Hi50 -f decklink -i 'DeckLink Mini Recorder' -c:v h264_nvenc -preset fast -pixel_format yuv420p -vf yadif -c:a aac -b:v 6M -b:a 320k -rtbufsize 8000 -f flv $rtmpServer #-c:v h264 -c:a aac -f mp4 test_$1.mp4
