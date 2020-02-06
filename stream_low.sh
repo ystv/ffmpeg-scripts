8f878d2f85d18ec06d0d6e472cd4a5e2fa699d96
Rhys Milling <rhys.milling@googlemail.com>
2020-02-06 22:41:01 +0000

    Create stream_low.sh

---
+++ stream_low.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ffmpeg  -format_code Hi50 -f decklink -i 'DeckLink Mini Recorder' -vf yadif -vcodec libx264 -preset ultrafast -tune zerolatency  -crf 20 -maxrate 10000k -bufsize 1000 -g 250 -f flv $rtmpServer
