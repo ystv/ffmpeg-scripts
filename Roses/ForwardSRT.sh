da4dc71550d34e42cb9a55af7eb8cd99e9a39fd2
cs-499243 <63954929+cs-499243@users.noreply.github.com>
2021-07-26 15:06:15 +0100

    Update and rename roses21RTMPForwarder.sh to Roses/ForwardSRT.sh

--- Roses/ForwardSRT.sh
+++ Roses/ForwardSRT.sh
@@ -1,11 +1,11 @@
-# 'FORWARDER SCRIPT' - RUN ON SPEED - roses21Forwarder
+# 'FORWARDER SCRIPT' - RUN ON SPEED
 # Application path (this would be the URL path of the application in OBS this would be everything after the domain)
-wowzaApp=""
+appPath=""
 # Endpoint so something like rtmp://example.com:1935
-wowzaEndpoint=""
+rtmpEndpoint=""
 
 homeIn="rtmp://stream.ystv.co.uk"
-homeOut="rtmp://stream.ystv.co.uk/internal/mixer"               # GET PROPER ADDRESS
+homeOut="rtmp://stream.ystv.co.uk/internal/mixer"               # CHANGE TO PROPER ADDRESS
 
 read -p "OB Pull point (e.g. mix/running): " obPullPoint
 read -p "LA1TV Play Path (e.g. ob1): " laPlayPath
@@ -16,8 +16,8 @@ while :; do
     ffmpeg -re -i "$homeIn/$obPullPoint" \
 -c copy -f flv -rtmp_flashver "flashver=FMLE/3.0\20(compatible;\20FMSc/1.0)" \
 -rtmp_playpath "$laPlayPath" \
--rtmp_app "$wowzaApp" \
-"rtmp://$wowzaEndpoint"
+-rtmp_app "$appPath" \
+"rtmp://$rtmpEndpoint"
 
     echo "Stream disconnected - trying again in 1 second"
     sleep 1
